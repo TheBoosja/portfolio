16a524dd1c985f0569bd636707dc6405b4aeece9
TheBoosja <mathias.iversen487@gmail.com>
2019-06-09 22:03:08 +0200

    Animated feature section

--- src/sections/Featured/index.js
+++ src/sections/Featured/index.js
@@ -1,41 +1,63 @@
 import React, { Component } from 'react';
-import itPilot from 'resources/imgs/itpilot-logo.png';
 import apromas from 'resources/imgs/apromas.png';
 import loadmax from 'resources/imgs/loadmax.png';
 import './featured.scss';
 
 class Featured extends Component {
-	renderLink(path, img, alt, isHeader) {
-		return (
-			<a className={'featured__link'} href={path}>
-				<img className='featured__img' src={img} alt={alt} />
-			</a>
-		)
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isHovering: {}
+		};
 	}
 
-	render() {
-		const Description = (props) => {
+	toggleHoverState(name, isHover = false) {
+		this.setState({
+			isHovering: {
+				...this.state.isHovering,
+				[name]: isHover
+			}
+		}, () => console.log('TOGGLE', name, this.state.isHovering[name]));
+	}
+
+	renderProject(path, img, alt, name, type, description) {
+		const handleHover = (isHover) => {
+			this.toggleHoverState(name, isHover);
+		}
+
+		const renderLink = (path, img, alt) => {
 			return (
-				<div className='featured__details'>
-					<div className='featured__name'>{props.name}</div>
-					<div className='featured__type'>{props.type}</div>
-					<div className='featured__desc'>{props.children}</div>
+				<a className={'featured__link'} href={path} onMouseEnter={() => handleHover(true)} onMouseLeave={() => handleHover(false)} key='2'>
+					<img className='featured__img' src={img} alt={alt} />
+				</a>
+			)
+		}
+
+		const renderDescription = (name, type, description) => {
+			const show = this.state.isHovering[name] ? ' featured__hover' : ''
+
+			return (
+				<div className={'featured__details' + show} key='1'>
+					<div className='featured__name'>{name}</div>
+					<div className='featured__type'>{type}</div>
+					<div className='featured__desc'>{description}</div>
 				</div>
 			)
 		}
 
+		return [
+			renderDescription(name, type, description),
+			renderLink(path, img, alt)
+		];
+	}
+
+	render() {
 		return (
 			<section className='featured'>
-					{/* {this.renderLink('https://www.itpilot.dk/', itPilot, 'itPilot', true)} */}
 				<div className='featured__projects'>
-					<Description name='Apromas' type='Website'>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</Description>
-					{this.renderLink('https://apromas.com/', apromas, 'Apromas')}
-					<Description name='LoadMax' type='Android App'>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</Description>
-					{this.renderLink('https://play.google.com/store/apps/details?id=com.loadmax', loadmax, 'LoadMax')}
+					{this.renderProject('https://apromas.com/', apromas, 'Apromas', 'Apromas', 'Website', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')}
+					{this.renderProject('https://play.google.com/store/apps/details?id=com.loadmax', loadmax, 'LoadMax', 'Loadmax', 'Android App', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')}
 				</div>
 			</section>
 		);
