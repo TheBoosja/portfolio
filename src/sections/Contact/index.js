e8ab3c71cb217a9d733707b2edf77436ba6e3fa9
TheBoosja <mathias.iversen487@gmail.com>
2019-06-11 20:16:08 +0200

    Added contact section

---
+++ src/sections/Contact/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './contact.scss';
+
+class Contact extends Component {
+	render() {
+		return (
+			<section className='contact'>
+				Hello
+			</section>
+		);
+	}
+}
+
+export default Contact;
